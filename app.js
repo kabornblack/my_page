1ea45611e67c20e17d0fb56909f91e5fea7202e0
kabornblack <shaibu.kabiru@email.com>
2023-05-31 17:19:35 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+const express =  require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
